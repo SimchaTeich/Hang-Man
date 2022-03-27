182b21a7e59c062cc98f500d7749d0df35d8ac8a
SimchaTeich <sim.teich@gmail.com>
2022-03-28 02:24:24 +0300

    clean halpers file

--- get_secret_word.py
+++
@@ -1,9 +0,0 @@
-def choose_word(file_path, index):
-    words = []
-    with open(file_path, "r") as f:
-        words = f.read().split()
-
-    num_of_diff_words = len(list(dict.fromkeys(words)))
-
-    return num_of_diff_words, words[(index-1) % len(words)]
-
--- hidden_word.py
+++
@@ -1,6 +0,0 @@
-def show_hidden_word(secret_word, old_letters_guessed):
-    return " ".join(l if l in old_letters_guessed else '_' for l in secret_word)
-
-def check_win(secret_word, old_letters_guessed):
-    return '_' not in show_hidden_word(secret_word, old_letters_guessed)
-
