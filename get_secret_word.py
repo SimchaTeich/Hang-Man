8ec9f21049fa1bb4b7ef618d449e75285de05e16
SimchaTeich <sim.teich@gmail.com>
2022-03-27 17:44:34 +0300

    update get_secret_word

--- get_secret_word.py
+++ get_secret_word.py
@@ -1,3 +1,7 @@
 secret_word = input("Please enter a word: ").lower()
+word_pattern = "_ " * len(secret_word)
 
-print("_ " * len(secret_word))
+# delete the last space
+word_pattern = word_pattern[:-1]
+
+print(word_pattern)
