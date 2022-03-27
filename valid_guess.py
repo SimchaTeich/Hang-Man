4bad46867f29b9812ca9be4729782ed170df846a
SimchaTeich <sim.teich@gmail.com>
2022-03-27 18:45:49 +0300

    change is_valis_input func to check_valid_input func

--- valid_guess.py
+++ valid_guess.py
@@ -1,3 +1,5 @@
-def is_valid_input(letter_guessed):
-    return len(letter_guessed) == 1 and letter_guessed.isalpha()
+def check_valid_input(letter_guessed, old_letters_guessed):
+    letter_guessed = letter_guessed.lower()
+    return len(letter_guessed) == 1 and letter_guessed.isalpha() \
+            and letter_guessed not in old_letters_guessed
 
