c0e020eb528aac023d75cf3d85ad2d7d717ebb40
SimchaTeich <sim.teich@gmail.com>
2022-03-27 14:12:50 +0300

    paint the open screen of the game

---
+++ open_screen.py
@@ -0,0 +1,16 @@
+from random import randint
+
+hang_man_open_screen = """  _    _                                         
+ | |  | |                                        
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |                      
+                     |___/"""
+min_tries = 1
+max_tries = 6
+
+print(hang_man_open_screen)
+print(randint(min_tries, max_tries))
+
