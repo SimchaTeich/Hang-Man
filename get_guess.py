3c69486860ae2d7cb76225a269d65a6ea65f75e4
SimchaTeich <sim.teich@gmail.com>
2022-03-27 18:16:46 +0300

    update get_guess.py to get one alphbetic char or print Error

--- get_guess.py
+++ get_guess.py
@@ -1,3 +1,19 @@
+# for input more then one letter but still alphabetic string
+ERROR_1 = "E1"
+
+# for one char than not alphabetic string
+ERROR_2 = "E2"
+
+# this is a combine of error 1 and error 2
+ERROR_3 = "E3"
+
 guessed_letter = input("Guess a letter: ").lower()
 
-print(guessed_letter)
+if len(guessed_letter) > 1 and guessed_letter.isalpha():
+    print(ERROR_1)
+elif len(guessed_letter) > 1:
+    print(ERROR_3)
+elif len(guessed_letter) == 1 and not guessed_letter.isalpha():
+    print(ERROR_2)
+else:
+    print(guessed_letter)
